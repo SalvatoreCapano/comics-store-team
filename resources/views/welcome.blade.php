3787c2eac3555f29c4cc6a7874878148f03bb86e
Iacopo Buffoni <itsmadpupp@outlook.it>
2023-03-28 16:36:33 +0200

    Dashboard

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
                 </svg>
             </div>
             <h1 class="display-5 fw-bold">
-                Welcome to Laravel+Bootstrap 5
+            Comics by Felo, Matte, Salvo, Luca, Iak
             </h1>
         </div>
     </div>
