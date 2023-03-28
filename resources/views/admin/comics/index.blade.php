d3078ccc60b6f99522a821848519b703c3066309
Salvatore Capano <capano_salvatore@libero.it>
2023-03-28 16:39:50 +0200

    View Index

---
+++ resources/views/admin/comics/index.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Index</title>
+</head>
+
+<body>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Descrizione</th>
+                <th scope="col">Prezzo</th>
+                <th scope="col">Quantita'</th>
+                <th scope="col">Azioni'</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($comics as $comic)
+                <tr>
+                    <th scope="row">{{ $comic->id }}</th>
+
+                    <td>{{ $comic->name }}</td>
+                    <td>{{ $comic->description }}</td>
+                    <td>{{ $comic->price }} $</td>
+                    <td>{{ $comic->quantity }}</td>
+                
+                    <td>
+                        <a href="{{ route('admin.projects.show', $project->id) }}" class="btn btn-primary">
+                            <i class="fa-solid fa-eye"></i>
+                        </a>
+                        <a href="{{ route('admin.projects.edit', $project->id) }}" class="btn btn-warning">
+                            <i class="fa-solid fa-wrench"></i>
+                        </a>
+                        <form class="d-inline-block" action="{{ route('admin.projects.destroy', $project->id) }}"
+                            method="POST" onsubmit="return confirm('Sei sicuro di voler eliminare questo progetto?');">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn btn-danger">
+                                <i class="fa-solid fa-trash-can"></i>
+                            </button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</body>
+
+</html>
